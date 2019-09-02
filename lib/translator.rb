0cde9298cfa3273510749025dfd41be667b0e7e4
Joelle Helm <joellenhelm@gmail.com>
2019-09-02 19:39:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,19 +1,12 @@
 require "yaml"
 
 def load_library(file)
-  jpart = {}
-  epart = {}
-  hash1 = {}
-  hash2 = {}
-  hash = {}
+  # jpart = {}
+  # epart = {}
+  # hash1 = {}
+  # hash2 = {}
+  # hash = {}
   emotes = YAML.load(File.open(File.join(File.dirname(__FILE__), 'emoticons.yml')))
-  # emotes.each do |a, b|
-  #   emote = b[0]
-  #   mean = b[1]
-  #   hash[:get_emoticon] << emote
-  #   hash[:get_meaning]
-  #   puts hash
-  # end
   work = emotes.invert
   work.each do |a, b|
     jpart[a[1]] = b
