105e743850ab7956c519561c2b52d42775dca3ba
Joelle Helm <joellenhelm@gmail.com>
2019-09-02 17:51:34 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,7 @@ require "yaml"
 def load_library(file)
 
 
-  hash = {"get_emoticon" => {YAML.load(File.open(File.join(File.dirname(__FILE__), 'emoticons.yml')))
+  hash = {"get_emoticon" => YAML.load(File.open(File.join(File.dirname(__FILE__), 'emoticons.yml')))
 end
 
 def get_japanese_emoticon
